5c075c2d1cce148030a9561da53512456ebe9528
Carlos Mecha <mecha.carlos@gmail.com>
2015-07-14 15:41:50 -0700

    Initial commit from local.

---
+++ NotifClientMacOS/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NotifClientMacOS
+//
+//  Created by Carlos Mecha on 7/14/15.
+//  Copyright (c) 2015 Carlos Mecha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
